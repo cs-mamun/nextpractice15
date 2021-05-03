5fa684676039faf097b346c150d510299fa9456d
cs-mamun <mukhula.mamun.cse@ulab.edu.bd>
2021-05-03 23:22:51 +0600

    2nd day done

--- src/App.js
+++ src/App.js
@@ -1,51 +1,121 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [personState,setPersonState] = useState({
     persons: [
       { name: "Dr. Mamun", age: 25, expert: "Ethical Hacking" },
       { name: "Dr. Mam", age: 27, expert: "Cybersecurity" },
       { name: "Dr. Babu", age: 29, expert: "Algorithm" },
-    ],
+    ]
+  });
+
+  const switchNameHandler = () => {
+    // console.log("clicked");
+    // this.state.persons[0].name = 'Spy Mamun';
+    setPersonState({
+      persons: [
+        { name: "Mamun PhD", age: 25, expert: "Ethical Hacking" },
+        { name: "Dr. Mam", age: 27, expert: "Cybersecurity" },
+        { name: "Babu", age: 28, expert: "Data Structure" },
+      ],
+    });
   };
-  render() {
-    return (
-      <div className="App">
-        <strong>Hey there! I am the boss who compiles all.</strong>
-        <button>-Switch Name-</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          expert={this.state.persons[0].expert}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          expert={this.state.persons[1].expert}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-          expert={this.state.persons[2].expert}
-        > Hello I am proprs Children</Person>
-      </div>
-    );
-  }
+
+  return (
+    <div className="App">
+      <strong>Hey there! I am the boss who compiles all.</strong>
+      <button onClick={switchNameHandler}>-Switch Name-</button>
+      <Person
+        name={personState.persons[0].name}
+        age={personState.persons[0].age}
+        expert={personState.persons[0].expert}
+      />
+      <Person
+        name={personState.persons[1].name}
+        age={personState.persons[1].age}
+        expert={personState.persons[1].expert}
+      />
+      <Person
+        name={personState.persons[2].name}
+        age={personState.persons[2].age}
+        expert={personState.persons[2].expert}
+      >
+        Hello I am props Children
+      </Person>
+    </div>
+  ); 
 }
 
 export default App;
 
-// function App() {
-// return (
-//   <div className="App">
-//     <strong>Hey there! I am the boss who compiles all.</strong>
-//     <Person name="Dr. Mamun" age="25" expert="Ethical Hacking" />
-//     <Person name="Dr. Mam" age="27" expert="Cybersecurity" />
-//     <Person name="Dr. Babu" age="29" expert="Algorithm"> Hello baby i am children </Person>
-//   </div>
-// );
+
+
+// class App extends Component {
+//   state = {
+//     persons: [
+//       { name: "Dr. Mamun", age: 25, expert: "Ethical Hacking" },
+//       { name: "Dr. Mam", age: 27, expert: "Cybersecurity" },
+//       { name: "Dr. Babu", age: 29, expert: "Algorithm" },
+//     ],
+//     cars: [
+//       {name: "Audi", year: 2021}
+//     ]
+//   };
+
+//   switchNameHandler = () => {
+//     // console.log("clicked");
+//     // this.state.persons[0].name = 'Spy Mamun';
+//     this.setState({
+//       persons: [
+//         { name: "Mamun PhD", age: 25, expert: "Ethical Hacking" },
+//         { name: "Dr. Mam", age: 27, expert: "Cybersecurity" },
+//         { name: "Babu", age: 28, expert: "Data Structure" },
+//       ]
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <strong>Hey there! I am the boss who compiles all.</strong>
+//         <button onClick={this.switchNameHandler}>-Switch Name-</button>
+//         <Person
+//           name={this.state.persons[0].name}
+//           age={this.state.persons[0].age}
+//           expert={this.state.persons[0].expert}
+//         />
+//         <Person
+//           name={this.state.persons[1].name}
+//           age={this.state.persons[1].age}
+//           expert={this.state.persons[1].expert}
+//         />
+//         <Person
+//           name={this.state.persons[2].name}
+//           age={this.state.persons[2].age}
+//           expert={this.state.persons[2].expert}
+//         >
+//           {" "}
+//           Hello I am props Children
+//         </Person>
+//       </div>
+//     );
+//   }
 // }
 
 // export default App;
+
+// // function App() {
+// // return (
+// //   <div className="App">
+// //     <strong>Hey there! I am the boss who compiles all.</strong>
+// //     <Person name="Dr. Mamun" age="25" expert="Ethical Hacking" />
+// //     <Person name="Dr. Mam" age="27" expert="Cybersecurity" />
+// //     <Person name="Dr. Babu" age="29" expert="Algorithm"> Hello baby i am children </Person>
+// //   </div>
+// // );
+// // }
+
+// // export default App;
