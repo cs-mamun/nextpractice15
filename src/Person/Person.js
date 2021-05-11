cbc4bcc83707d53723956c258e87ad0f503a9583
cs-mamun <mukhula.mamun.cse@ulab.edu.bd>
2021-05-11 23:47:00 +0600

    bluk

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,7 @@ import './Person.css'
 const person = (props) =>{
     return (
         <div className="Person">
-            <h2>Greetings! I am form the react application</h2>
+            <h2>Greetings! I am form another component from this application.</h2>
 
             <h5 onClick={props.click}> Hi, I am {props.name} and {props.age} years old and my hobby is {props.expert}</h5>
             <p>{props.children}</p>
